029fd41a96659f4db5b81fbba29b4e3ac3311abe
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-03 16:57:34 -0700

    Calculates total_charge for Passenger's Trips

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,14 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+
+  def total_charge
+    # Find the trips passenger took ,
+    sum = 0
+    self.trips.each do |trip|
+      sum += trip.cost
+    end
+    return sum / 100
+  end
+
 end
