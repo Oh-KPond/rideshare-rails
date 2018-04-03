f3caf2db1417ad6f547d12c61f4592cf4b8ea23d
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-02 18:06:08 -0700

    Uses scaffold to create controllers, views, helpers, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'trips#index'
+
+  get '/trips', to: 'trips#index', as: 'trips'
+  get 'trips/new', to: 'trips#new', as: 'new_trip'
+  post '/trips', to: 'trips#create'
+  get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+  get '/trips/:id', to: 'trips#show', as: 'trip'
+  patch '/trips/:id', to: 'trips#update'
+  delete '/trips/:id', to: 'trips#destroy'
 end
