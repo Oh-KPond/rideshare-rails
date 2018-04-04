5f9966cfe3e06b5a9d68b9fc6e5e72cb800cf1f6
Madaleine Shields <maddiev.shields@gmail.com>
2018-04-03 17:11:39 -0700

    Validate User Input for Creating a Passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,11 @@
 class Passenger < ApplicationRecord
   has_many :trips
 
+  validates :name, presence: true
+  validates :phone_num, presence: true
+
+
+
 
   def total_charge
     # Find the trips passenger took ,
